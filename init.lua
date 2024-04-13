2dcd38c6ac85fc7eb7aba1434efab4e675ff1aaa
Rahul <rahulehh@icloud.com>
2024-04-13 18:56:12 +0530

    install cattpuccin and telescope.nvim
    in this commit, the default cattpuccin colorscheme is implemented and keymaps are mapped for telescope funtionalities.

--- init.lua
+++ init.lua
@@ -1,5 +1,19 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+--[[
 
+Two space chararacter for ever tab space and intendation
+
+]]--
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+--[[
+
+Lazy.nvim installation
+
+]]--
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
@@ -12,9 +26,26 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {}
+local plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { 'nvim-telescope/telescope.nvim', tag = '0.1.6', dependencies = { 'nvim-lua/plenary.nvim' } }
+}
 
 local opts = {}
 
 require("lazy").setup(plugins, opts)
 
+--[[
+
+Keymaps for Telescope.nvim
+`<leader>` means `\`
+
+]]--
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
