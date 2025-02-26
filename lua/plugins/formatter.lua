7a02994a2f4f604c771895fa9d74219475cc9ce8
Rahul <rahulehh@icloud.com>
2025-02-26 22:41:50 +0530

    fix: flatten formatter lists in conform.nvim
    also tweak some format behaviour

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -7,16 +7,16 @@ return {
 		conform.setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-				svelte = { { "prettierd", "prettier" } },
-				javascript = { { "prettierd", "prettier" } },
-				typescript = { { "prettierd", "prettier" } },
-				javascriptreact = { { "prettierd", "prettier" } },
-				typescriptreact = { { "prettierd", "prettier" } },
-				vue = { { "prettierd", "prettier" } },
-				json = { { "prettierd", "prettier" } },
-				graphql = { { "prettierd", "prettier" } },
+				svelte = { "prettierd", "prettier" },
+				javascript = { "prettierd", "prettier" },
+				typescript = { "prettierd", "prettier" },
+				javascriptreact = { "prettierd", "prettier" },
+				typescriptreact = { "prettierd", "prettier" },
+				vue = { "prettierd", "prettier" },
+				json = { "prettierd", "prettier" },
+				graphql = { "prettierd", "prettier" },
 				go = { "gofmt" },
-				markdown = { { "prettierd", "prettier" } },
+				markdown = { "prettierd", "prettier" },
 				erb = { "htmlbeautifier" },
 				html = { "htmlbeautifier" },
 				bash = { "beautysh" },
@@ -25,12 +25,8 @@ return {
 				rust = { "rustfmt" },
 				yaml = { "yamlfix" },
 				toml = { "taplo" },
-				css = { { "prettierd", "prettier" } },
-				scss = { { "prettierd", "prettier" } },
-			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_format = "fallback",
+				css = { "prettierd", "prettier" },
+				scss = { "prettierd", "prettier" },
 			},
 		})
 
@@ -39,6 +35,7 @@ return {
 				lsp_fallback = true,
 				async = false,
 				timeout_ms = 500,
+				stop_after_first = true,
 			})
 		end, { desc = "Format file or range (in visual mode)" })
 	end,
